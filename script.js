fb9d9073bc11bc1947580fb8db23d4e75872b21c
Miguel Guzman <mguzmanojeda@zalando-840151.corp.ad.zalando.net>
2024-11-16 01:31:57 +0100

    Add films pages

--- script.js
+++ script.js
@@ -17,4 +17,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Update GIFs if the window is resized
     window.addEventListener('resize', updateBackgrounds);
+
+    // Load external menu
+    fetch('menu.html')
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById('menu-container').innerHTML = html;
+        });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Load external menu
+    fetch('menu.html')
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById('menu-container').innerHTML = html;
+
+            // Attach event listener to burger icon
+            const burgerIcon = document.querySelector('.burger-icon');
+            const menuLinks = document.getElementById('menuLinks');
+            if (burgerIcon && menuLinks) {
+                burgerIcon.addEventListener('click', () => {
+                    menuLinks.classList.toggle('show'); // Toggle menu visibility
+                });
+            }
+        });
 });
